566146e250184b405972e32755d7e986d077d7e1
unknown <manafikhiamjad@gmail.com>
2025-07-03 16:10:32 +0300

    edited env variables

--- src/lib/db.js
+++ src/lib/db.js
@@ -5,6 +5,7 @@ const connectionConfig = {
     user:process.env.DATABASE_USER,
     password:process.env.DATABASE_PASSWORD,
     database:process.env.DATABASE_NAME,
+    database:process.env.DATABASE_PORT
 }
    
 const pool = mysql.createPool(connectionConfig);
