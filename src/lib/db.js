2cd9565786b8f0b00406224068fdcb06dfcfb07b
unknown <manafikhiamjad@gmail.com>
2025-07-03 19:50:36 +0300

    edited db.js credentials

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,13 +1,13 @@
-import mysql from "mysql2/promise"
+import mysql from "mysql2/promise";
 
 const connectionConfig = {
-    host:process.env.DATABASE_HOST,
-    user:process.env.DATABASE_USER,
-    password:process.env.DATABASE_PASSWORD,
-    database:process.env.DATABASE_NAME,
-    database:process.env.DATABASE_PORT
-}
-   
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  port: Number(process.env.DATABASE_PORT),
+};
+
 const pool = mysql.createPool(connectionConfig);
 
 export async function query(sql, params) {
