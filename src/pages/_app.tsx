ad4c3363d47e95d5871821ef20c766acdf997f13
unknown <manafikhiamjad@gmail.com>
2025-07-03 20:36:05 +0300

    edit _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import NextNProgress from "nextjs-progressbar"
-
+import NextNProgress from "nextjs-progressbar";
 
 export default function App({ Component, pageProps }: AppProps) {
-  
-  return( 
-    <div>
-      <NextNProgress/>
-      <Component {...pageProps} />;
-    </div>
-  )
+  return (
+    <>
+      <NextNProgress
+        color="#29D"              // Optional: bar color
+        startPosition={0.3}       // Optional: where the bar starts
+        stopDelayMs={200}         // Optional: delay before stopping
+        height={3}                // Optional: bar height in px
+        showOnShallow={true}      // Optional: show on shallow routing
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
