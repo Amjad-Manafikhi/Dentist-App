b6ed68489ed2aec339615f7f00ebeb1c506703fb
unknown <manafikhiamjad@gmail.com>
2025-07-23 22:50:34 +0300

    profile page

---
+++ src/pages/profile.tsx
@@ -0,0 +1,9 @@
+export default function profile(){
+
+    return(
+        <div className="w-screen h-screen flex justify-center items-center">
+
+            <h1 className="font-bold text-2xl">coming soon</h1>
+        </div>
+    ) 
+}
