34eeb4de205768280e9798848defd325b59b2d0b
unknown <manafikhiamjad@gmail.com>
2025-08-03 23:31:49 +0300

    solved deployment errors

--- src/pages/api/auth/logout.tsx
+++ src/pages/api/auth/logout.tsx
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { deleteSession, getSession } from '@/lib/session';
+import { deleteSession } from '@/lib/session';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'DELETE') {
